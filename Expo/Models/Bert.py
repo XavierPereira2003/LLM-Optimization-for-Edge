ae2b6d6f0ca92f6aded1732032be399b3ef298ac
Deon Xavier <deonxavier2019@gmail.com>
2024-10-27 18:37:28 +0530

    Add initial code for Expo Models
    This commit adds the initial code for the Expo Models. It includes the implementation of the `__init__.py` file and the state.json files for the Boolq, copa, Boolq_and_copq models. It also includes the implementation of the `service.proto` file, `Bert.py`, `service_pb2.py`, `CoPA.py`, `Boolq.py`, and `test.ipynb` files.

---
+++ Expo/Models/Bert.py
@@ -0,0 +1,51 @@
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import logging
+import os
+import sys
+
+log_path="./logs/"
+os.makedirs("./logs/", exist_ok=True) 
+logging.basicConfig(
+    level=logging.DEBUG,  
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("./logs/bert.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+
+class Classifier:
+    def __init__(self) -> None:
+        self.logger=logging.getLogger("Bert_logger")
+        model_path =os.path.join(os.path.dirname(os.path.abspath(__file__)), "Bert_classifier")
+
+        self.logger.debug(f"File Path {model_path}")
+
+        self.classifier_model=AutoModelForSequenceClassification.from_pretrained(model_path)
+        self.tokenizer=AutoTokenizer.from_pretrained(model_path)
+        
+        self.logger.info("Model and Tokenizer is loaded")
+        
+
+    def classify_text(self, input_text:str)->int:
+        self.logger.info("Classifying")
+        # Tokenize the input, specifying return_tensors='pt' only for the tokenizer
+        inputs = self.tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
+        self.logger.debug(f"Tokenized the input \"{input_text}\"")
+        with torch.no_grad():
+            outputs = self.classifier_model(**inputs) 
+
+        # Get predictions
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits, dim=-1).item()  
+        self.logger.debug(f"Generated the output '{predicted_class}'")
+        return int(predicted_class)
+    
+
+if __name__=="__main__":
+    inputs="do good samaritan laws protect those who help at an accident"
+    classifier=Classifier()
+    predicted_class=classifier.classify_text(inputs)
+    print(predicted_class)
---
+++ Expo/Models/Boolq.py
@@ -0,0 +1,59 @@
+#Model Libraries
+import os
+import torch
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from peft import PeftModel, get_peft_model, LoraConfig
+import logging
+import os
+import sys
+from datasets import load_from_disk
+import random
+# Configure logging to current directory
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("./logs/boolq_daemon_service.log"),  # Log file in current directory
+ #       logging.StreamHandler(sys.stdout)  # Also log to console
+    ]
+)
+
+
+
+class BoolqModel:
+    def __init__(self) -> None:
+        self.logger= logging.getLogger('BoolqLogger')
+        
+        self.model_name = "google/flan-t5-small"
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name)
+        self.model_tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.logger.debug("Model and Tokenizer is loaded")
+
+        # Load the LoRA model
+        self.lora_model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "flan_t5_boolq_lora_saved")
+        self.lora_config = LoraConfig.from_pretrained(self.lora_model_path)
+        self.model = get_peft_model(self.model, self.lora_config)
+
+    def predict(self, prompt:str) -> str:
+        self.logger.debug(f"Input text: {prompt}")
+        inputs=self.model_tokenizer(
+                prompt,
+                max_length=256,
+                padding="max_length",
+                truncation=True,
+                return_tensors="pt"
+        )
+        with torch.no_grad():
+            outputs=self.model.generate(**inputs)
+        pred = self.model_tokenizer.decode(outputs[0], skip_special_tokens=True).lower()
+        self.logger.debug(f"Prediciton:{pred}")
+        return pred
+    
+if __name__=="__main__":
+    boolq=load_from_disk(os.path.join(os.path.dirname(os.path.abspath(__file__)), "Boolq"))
+
+    inputs=boolq[random.randint(0,9427)]['input_text']
+    Boolq=BoolqModel()
+
+    outputs=Boolq.predict(inputs)
+    print(outputs)
